2d7bee3f6e9a75a2050d24e8449809694a4cafc8
abdullll21 <macmod.abheu22@southernchristiancollege.edu.ph>
2025-04-09 14:14:08 +0800

    go

---
+++ ResmaActivity/ResmaActivity/Conn.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ResmaActivity
+{
+    public class Conn
+    {
+        
+        
+            public SqlConnection connection;
+            public Conn()
+            {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();
+
+                var connectionStrings = configuration.GetConnectionString("DefaultConnection2");
+                connection = new SqlConnection(connectionStrings);
+            }
+        
+    }
+}
